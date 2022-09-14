6f44ccebb20a123a3fb3e24d45ca99b621d7e548
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-09-15 00:25:51 +0600

    final commit

--- history.js
+++ history.js
@@ -1,8 +1,8 @@
-const histories = document.getElementById("histories");
+const histories = document.getElementById('histories');
 
 function addHistory(questionText, timeTaken, errorCount) {
-  const newRow = document.createElement("div");
-  newRow.classList.add("card");
+  const newRow = document.createElement('div');
+  newRow.classList.add('card');
 
   newRow.innerHTML = `
   <h3>${questionText}</h3>
@@ -14,24 +14,26 @@ function addHistory(questionText, timeTaken, errorCount) {
 
   histories.appendChild(newRow);
 
-  let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+  let previousTests = JSON.parse(localStorage.getItem('testHistory')) || [];
   previousTests.push({ questionText, timeTaken, errorCount });
-  localStorage.setItem("testHistory", JSON.stringify(previousTests));
+  localStorage.setItem('testHistory', JSON.stringify(previousTests));
 
   displayHistory();
 }
 
 function displayHistory() {
-  histories.innerHTML = "";
-  const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+  histories.innerHTML = '';
+  const previousTests = JSON.parse(localStorage.getItem('testHistory')) || [];
 
   previousTests.forEach((test) => {
-    const newRow = document.createElement("div");
-    newRow.classList.add("card");
+    const newRow = document.createElement('div');
+    newRow.classList.add('card');
 
     newRow.innerHTML = `
   <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+  <p>You took: <span class="bold">${Math.round(
+    test.timeTaken
+  )}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
 
